a2636eb4d6de40908d4a7cbc665d2a7fb3097377
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-16 20:39:46 +0000

    Automatically backed up by Learn

--- some.rb
+++ some.rb
@@ -17,16 +17,4 @@ holiday_hash = {
    
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-<<<<<<< HEAD
-x = 
-holiday_hash.collect do |key, value|
-  value.find_all do |key, value|
-    key if value.include?("BBQ")
-  end
-end.flatten
 
-puts x
-=======
-
-puts holiday_hash.keys
->>>>>>> 17fd8cca207a8b23398309b0b700e77d031ac4bf
