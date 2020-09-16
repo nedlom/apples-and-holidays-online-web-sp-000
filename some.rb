6769bc4e4d980253bde7b49d198a6953df29a683
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-16 20:41:33 +0000

    Automatically backed up by Learn

--- some.rb
+++ some.rb
@@ -18,3 +18,7 @@ holiday_hash = {
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+holiday_hash.each do |key, value|
+  value.each do |key, value|
+    value.include
+end
